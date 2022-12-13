ba29447ed15c997a3b4a69c80d660b4d579490bc
luvittor <>
2022-12-13 09:18:17 -0300

    first commit

---
+++ database/migrations/2022_12_12_221935_criar_tabela_clientes.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('clientes', function (Blueprint $table) {
+            $table->id();
+            $table->string('cpf');
+            $table->string('nome');
+            $table->date('data_nascimento');
+            $table->enum('sexo', array('M', 'F'));
+            $table->string('endereco');
+            $table->string('estado');
+            $table->string('cidade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('clientes');
+    }
+};
+
